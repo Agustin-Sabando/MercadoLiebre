0cd59ff3e34818ad72ea15b2c75623c824b5e13f
Agustin-Sabando <agustinsabando18@gmail.com>
2022-07-12 17:07:02 -0300

    Subiendo la cuarta parte del trabajo de mercado liebre

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, 'public')))
+/* app.use(express.static(path.join(__dirname, 'public'))) */
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
 app.get('/ofertas', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'ofertas.html')))
