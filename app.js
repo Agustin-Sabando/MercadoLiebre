8fa3e63e2c8740e88063360c7f2a80516af6d1d8
Agustin-Sabando <agustinsabando18@gmail.com>
2022-07-21 17:48:11 -0300

    Subiendo la 5ta parte del trabajo práctico mercado liebre

--- app.js
+++ app.js
@@ -6,12 +6,7 @@ const port = 3000
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
-app.get('/ofertas', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'ofertas.html')))
-app.get('/tiendasOficiales', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'tiendasOficiales.html')))
-app.get('/vender', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'vender.html')))
-app.get('/ayuda', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'ayuda.html')))
-app.get('/creaTuCuenta', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'crearCuenta.html')))
-app.get('/ingresá', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'ingresar.html')))
-app.get('/misCompras', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'misCompras.html')))
+app.get('/crearCuenta', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')))
+app.get('/ingresar', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')))
 
 app.listen(port, () => console.log(`Servidor abierto en http://localhost:${port}`))
